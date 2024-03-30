54d036877eb62259a700ca422ef0caaec4c1d2f7
Gabriel <gabrielgapseu@gmail.com>
2024-03-30 11:20:01 -0300

    Buidl: database configs

---
+++ src/db/conn.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USERNAME, 
+    process.env.DB_PASSWORD, 
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: "mysql"
+        }
+    )
+
+async function connectToDatabase(){
+    try {
+        await sequelize.authenticate()
+        console.log("Successfully connected")
+        await sequelize.sync()
+        console.log("Sucessfully synconized")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { connectToDatabase, sequelize }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { connectToDatabase } from "./db/conn.js";
 import { bairroRouter } from "./routes/bairroRoutes.js";
 
 const app = express()
-const porta = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
     res.status(400).json({ error: '400 HTTP, verifique o caminho'})
 })
 
-app.listen(porta, () => {
-    console.log(`App rodando na porta ${porta}!`)
+connectToDatabase()
+.then(()=> {
+    app.listen(port)
 })
