aae679e2c34c33e79d963d0bf51468bfa7665bd6
Gabriel <gabrielgapseu@gmail.com>
2024-03-30 18:00:48 -0300

    Fix: bugfix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors"
 import { connectToDatabase } from "./db/conn.js";
 import { neighborhoodRouter } from "./routes/neighborhoodRoutes.js";
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173"}))
 
 app.use(express.json())
 
